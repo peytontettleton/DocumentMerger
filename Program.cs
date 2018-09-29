4be02d85243565ba31367770bd7764886483bb0c
peytontettleton <36649600+peytontettleton@users.noreply.github.com>
2018-09-28 21:55:01 -0500

    Creation

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DocumentMerger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Document Merger");
+            Console.WriteLine("Name of the First Document: ");
+            string Document1 = Console.ReadLine();
+
+        }
+    }
+}
